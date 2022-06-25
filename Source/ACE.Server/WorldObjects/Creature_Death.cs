e3f31d9ef786f34472970ac3f1dd7682be9e5bb8
AcPvP <89650671+AcPvP@users.noreply.github.com>
2022-06-25 12:40:36 -0400

    Log PK Kills to ace_pk_kills DB

--- Source/ACE.Server/WorldObjects/Creature_Death.cs
+++ Source/ACE.Server/WorldObjects/Creature_Death.cs
@@ -482,7 +482,29 @@ namespace ACE.Server.WorldObjects
                 var isPKLdeath = player.IsPKLiteDeath(killer);
 
                 if (isPKdeath)
+                {
                     corpse.PkLevel = PKLevel.PK;
+
+                    if(corpse != null && corpse.VictimId != null)
+                    {
+                        uint? victimMonarchId = null;
+                        uint? killerMonarchId = null;
+                        var killerAllegiance = AllegianceManager.GetAllegiance(PlayerManager.FindByGuid(killer.Guid));
+                        var victimAllegiance = AllegianceManager.GetAllegiance(PlayerManager.FindByGuid(new ObjectGuid(corpse.VictimId.Value)));
+
+                        if(killerAllegiance != null)
+                        {
+                            killerMonarchId = killerAllegiance.MonarchId;
+                        }
+
+                        if (victimAllegiance != null)
+                        {
+                            victimMonarchId = victimAllegiance.MonarchId;
+                        }
+
+                        DatabaseManager.PKKills.CreateKill((uint)corpse.VictimId, (uint)killer.Guid.Full, victimMonarchId, killerMonarchId);
+                    }
+                }
 
                 if (!isPKdeath && !isPKLdeath)
                 {
