f3422cbaa983664fd4123042ef01cd6b3e6958d3
Dekaru <dekaru@protonmail.com>
2022-06-28 07:53:53 -0300

    Scroll level chances adjustment for Infiltration ruleset.

--- Source/ACE.Server/Factories/Tables/ScrollLevelChance.cs
+++ Source/ACE.Server/Factories/Tables/ScrollLevelChance.cs
@@ -53,13 +53,61 @@ namespace ACE.Server.Factories.Tables
 
         public static int Roll(TreasureDeath profile)
         {
-            // shortcut
-            if (profile.Tier >= 5)
-                return 7;
-
             var table = scrollLevelChances[profile.Tier - 1];
 
             return table.Roll(profile.LootQualityMod);
+        }
+
+        static ScrollLevelChance()
+        {
+            if (Common.ConfigManager.Config.Server.WorldRuleset <= Common.Ruleset.Infiltration)
+            {
+                T1_ScrollLevelChances = new ChanceTable<int>()
+                {
+                    ( 1, 0.60f ),
+                    ( 2, 0.30f ),
+                    ( 3, 0.10f ),
+                };
+
+                T2_ScrollLevelChances = new ChanceTable<int>()
+                {
+                    ( 3, 0.58f ),
+                    ( 4, 0.28f ),
+                    ( 5, 0.14f ),
+                };
+
+                T3_ScrollLevelChances = new ChanceTable<int>()
+                {
+                    ( 4, 0.10f ),
+                    ( 5, 0.80f ),
+                    ( 6, 0.10f ),
+                };
+
+                T4_ScrollLevelChances = new ChanceTable<int>()
+                {
+                    ( 5, 0.75f ),
+                    ( 6, 0.25f ),
+                };
+
+                T5_T8_ScrollLevelChances = new ChanceTable<int>()
+                {
+                    ( 5, 0.20f ),
+                    ( 6, 0.80f ),
+                };
+
+                // we have to refresh this list or it will still contain the previous values.
+                scrollLevelChances = new List<ChanceTable<int>>()
+                {
+                    T1_ScrollLevelChances,
+                    T2_ScrollLevelChances,
+                    T3_ScrollLevelChances,
+                    T4_ScrollLevelChances,
+                    T5_T8_ScrollLevelChances,
+                    T5_T8_ScrollLevelChances,
+                    T5_T8_ScrollLevelChances,
+                    T5_T8_ScrollLevelChances,
+                };
+            }
         }
     }
 }
