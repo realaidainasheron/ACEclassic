a504a9158cc8c920eb358f87c005d6d76e8397b8
Dekaru <dekaru@protonmail.com>
2022-06-28 08:30:29 -0300

    Split Infiltration ruleset scroll chances for t5 and t6.

--- Source/ACE.Server/Factories/Tables/ScrollLevelChance.cs
+++ Source/ACE.Server/Factories/Tables/ScrollLevelChance.cs
@@ -34,6 +34,10 @@ namespace ACE.Server.Factories.Tables
             ( 7, 0.50f )
         };
 
+        private static ChanceTable<int> T5_ScrollLevelChances;
+
+        private static ChanceTable<int> T6_ScrollLevelChances;
+
         private static ChanceTable<int> T5_T8_ScrollLevelChances = new ChanceTable<int>()
         {
             ( 7, 1.00f )
@@ -57,7 +61,6 @@ namespace ACE.Server.Factories.Tables
 
             return table.Roll(profile.LootQualityMod);
         }
-
         static ScrollLevelChance()
         {
             if (Common.ConfigManager.Config.Server.WorldRuleset <= Common.Ruleset.Infiltration)
@@ -78,21 +81,27 @@ namespace ACE.Server.Factories.Tables
 
                 T3_ScrollLevelChances = new ChanceTable<int>()
                 {
+                    ( 4, 0.62f ),
+                    ( 5, 0.26f ),
+                    ( 6, 0.12f ),
+                };
+
+                T4_ScrollLevelChances = new ChanceTable<int>()
+                {
                     ( 4, 0.10f ),
                     ( 5, 0.80f ),
                     ( 6, 0.10f ),
                 };
 
-                T4_ScrollLevelChances = new ChanceTable<int>()
-                {
-                    ( 5, 0.75f ),
-                    ( 6, 0.25f ),
-                };
-
-                T5_T8_ScrollLevelChances = new ChanceTable<int>()
+                T5_ScrollLevelChances = new ChanceTable<int>()
                 {
                     ( 5, 0.20f ),
                     ( 6, 0.80f ),
+                };
+
+                T6_ScrollLevelChances = new ChanceTable<int>()
+                {
+                    ( 6, 1.00f ),
                 };
 
                 // we have to refresh this list or it will still contain the previous values.
@@ -102,10 +111,10 @@ namespace ACE.Server.Factories.Tables
                     T2_ScrollLevelChances,
                     T3_ScrollLevelChances,
                     T4_ScrollLevelChances,
-                    T5_T8_ScrollLevelChances,
-                    T5_T8_ScrollLevelChances,
-                    T5_T8_ScrollLevelChances,
-                    T5_T8_ScrollLevelChances,
+                    T5_ScrollLevelChances,
+                    T6_ScrollLevelChances,
+                    T6_ScrollLevelChances,
+                    T6_ScrollLevelChances,
                 };
             }
         }
