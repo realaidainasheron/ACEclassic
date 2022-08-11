0e2adfdfc7048bad15ad8da4dc847146d587c984
AcPvP <89650671+AcPvP@users.noreply.github.com>
2022-08-11 18:15:29 -0400

    Add a TurnTo limit to avoid infinite turn attempts on magic - from Foredawn commit
    https://github.com/ACRealms/Foredawn.Server/commit/e060b5b9219c34e13391fadc1a433f90f81caf6f

--- Source/ACE.Server/Managers/PropertyManager.cs
+++ Source/ACE.Server/Managers/PropertyManager.cs
@@ -599,7 +599,8 @@ namespace ACE.Server.Managers
                 ("rares_max_days_between", new Property<long>(45, "for rares_real_time_v2: the maximum number of days a player can go before a rare is generated on rare eligible creature kills")),
                 ("rares_max_seconds_between", new Property<long>(5256000, "for rares_real_time: the maximum number of seconds a player can go before a second chance at a rare is allowed on rare eligible creature kills that did not generate a rare")),
                 ("teleport_visibility_fix", new Property<long>(0, "Fixes some possible issues with invisible players and mobs. 0 = default / disabled, 1 = players only, 2 = creatures, 3 = all world objects")),
-                ("max_level", new Property<long>(275, "Set the max character level."))
+                ("max_level", new Property<long>(275, "Set the max character level.")),
+                ("windup_turn_retry_number", new Property<long>(0, "Fixes turning forever during windup. 0 = default / disabled, 1 = retry one time, 2 = retry two times, ..."))
                 );
 
         public static readonly ReadOnlyDictionary<string, Property<double>> DefaultDoubleProperties =
