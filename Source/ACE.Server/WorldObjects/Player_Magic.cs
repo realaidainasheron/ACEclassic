8f0f1f2c7a2daa2a4b12a0b0f5ae74625599e93b
AcPvP <89650671+AcPvP@users.noreply.github.com>
2022-08-24 11:09:08 -0400

    Add null checks to DoCastSpell_Inner where we allow targeting players in portal space + some logging changes to windup max turnto code

--- Source/ACE.Server/WorldObjects/Player_Magic.cs
+++ Source/ACE.Server/WorldObjects/Player_Magic.cs
@@ -237,6 +237,7 @@ namespace ACE.Server.WorldObjects
                 }
                 else
                     MagicState.PendingTurnRelease = true;
+                }
             }
         }
 
